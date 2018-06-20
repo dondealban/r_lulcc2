d19354689679453be9fe545714a83f875c614da0
simon <simon@yamunotri.localdomain>
2018-06-20 19:29:20 +0100

    updated demo script

--- lulcc/demo/gmd-paper.R
+++ lulcc/demo/gmd-paper.R
@@ -1,7 +1,8 @@
-## July 2015
+## July 2015, updated June 2018
 
-## Demo script to run the code and produce the figures shown in the Geoscientific
-## Model Development Discussions paper
+## Demo script to run the code and produce the figures shown in
+## the Geoscientific Model Development paper
+devtools::load_all("lulcc")
 
 library(RColorBrewer)  ## required for plotting
 
@@ -9,17 +10,16 @@ library(RColorBrewer)  ## required for plotting
 data(pie)
 
 ## observed maps
-obs <- ObsLulcRasterStack(x=pie,
-                          pattern="lu", 
-                          categories=c(1,2,3), 
-                          labels=c("Forest","Built","Other"), 
-                          t=c(0,6,14))
+lu <- DiscreteLulcRasterStack(x=stack(pie[1:3]),
+                               categories=c(1,2,3),
+                               labels=c("Forest","Built","Other"),
+                               t=c(0,6,14))
 
 ## show object
-obs
+lu
 
-## figure 3
-p <- plot(obs,
+## figure
+p <- plot(lu,
           between=list(x=0,y=0),
           par.settings=list(axis.line=list(col="black"),
             strip.background=list(col="lightgrey")),
@@ -40,35 +40,71 @@ p
 ## dev.off()
 
 ## cross tabulate change between two time points
-crossTabulate(obs, times=c(0,14))
+crossTabulate(lu, times=c(0,14))
 
 ## explanatory variables
-ef <- ExpVarRasterList(x=pie, pattern="ef")
+ix <- data.frame(var=c("ef_001","ef_002","ef_003"),
+                 yr=c(0,0,0),
+                 dynamic=c(F,F,F))
+ef <- ExpVarRasterStack(x=stack(pie[4:6]), index=ix)
 ef
 
 ## resample ef (not necessary in this example)
-ef <- resample(ef, obs)
+ef <- resample(ef, lu)
 
 ## fit statistical models
-part <- partition(x=obs[[1]], size=0.1, spatial=TRUE)
-train.data <- getPredictiveModelInputData(obs=obs, ef=ef, cells=part[["train"]])
+part <- partition(x=lu, size=0.1, spatial=TRUE, t=0)
 
-forms <- list(Built ~ ef_001+ef_002+ef_003,
-              Forest ~ ef_001+ef_002,
-              Other ~ ef_001+ef_002)
+train.data <- getPredictiveModelInputData(lu=lu,
+                                          ef=ef,
+                                          cells=part[["train"]],
+                                          t=0)
 
-glm.models <- glmModels(formula=forms, family=binomial, data=train.data, obs=obs)
-rpart.models <- rpartModels(formula=forms, data=train.data, obs=obs)
-rf.models <- randomForestModels(formula=forms, data=train.data, obs=obs)
+test.data  <- getPredictiveModelInputData(lu=lu,
+                                          ef=ef,
+                                          cells=part[["test"]],
+                                          t=0)
+
+library(randomForest)
+library(rpart)
+
+built.form  <- as.formula("Built ~ ef_001 + ef_002 + ef_003")
+built.rf    <- randomForest(built.form, data=train.data)
+built.glm   <- glm(built.form, family=binomial, data=train.data)
+## built.rpart <- rpart(built.form, method="class", data=train.data)
+
+forest.form <- as.formula("Forest ~ ef_001 + ef_002")
+forest.rf   <- randomForest(forest.form, data=train.data)
+forest.glm  <- glm(forest.form, data=train.data)
+
+other.form  <- as.formula("Other ~ ef_001 + ef_002")
+other.rf    <- randomForest(other.form, data=train.data)
+other.glm   <- glm(other.form, data=train.data)
+
+rf.mods  <- new("PredictiveModelList",
+                models=list(forest.rf, built.rf, other.rf),
+                categories=lu@categories,
+                labels=lu@labels)
+
+glm.mods <- PredictiveModelList(models=list(forest.glm, built.glm, other.glm),
+                                categories=lu@categories,
+                                labels=lu@labels)
+
+rf.pred  <- PredictionList(models=rf.mods, newdata=test.data)
+rf.perf  <- PerformanceList(pred=rf.pred, measure="rch")
+
+glm.pred <- PredictionList(models=glm.mods, newdata=test.data)
+glm.perf <- PerformanceList(pred=glm.pred, measure="rch")
 
 ## create suitability maps
 all.data <- as.data.frame(x=ef, cells=part[['all']])
-probmaps <- predict(object=glm.models, newdata=all.data, data.frame=TRUE)
-points <- rasterToPoints(obs[[1]], spatial=TRUE)
+probmaps <- predict(object=glm.mods, newdata=all.data, data.frame=TRUE)
+points <- rasterToPoints(lu[[1]], spatial=TRUE)
 probmaps <- SpatialPointsDataFrame(points, probmaps)
-r <- rasterize(x=probmaps, y=obs[[1]], field=names(probmaps))
+r <- rasterize(x=probmaps, y=lu[[1]], field=names(probmaps))
 
 ## figure 4
+library(RColorBrewer)
 p <- rasterVis::levelplot(r,
                           layout=c(3,1),
                           margin=FALSE,
@@ -86,38 +122,13 @@ p
 ## print(p)
 ## dev.off()
 
-## test ability of models to predict location of forest, urban, other land uses in testing partition
-test.data  <- getPredictiveModelInputData(obs=obs, ef=ef, cells=part[["test"]])
-glm.pred <- PredictionList(models=glm.models, newdata=test.data)
-glm.perf <- PerformanceList(pred=glm.pred, measure="rch")
 
-rpart.pred <- PredictionList(models=rpart.models, newdata=test.data)
-rpart.perf <- PerformanceList(pred=rpart.pred, measure="rch")
+## test ability of models to predict location of urban gain
+## 1985 -> 1991
+part <- rasterToPoints(lu[[1]], fun=function(x) x != 2, spatial=TRUE)
+test.data <- getPredictiveModelInputData(lu=lu, ef=ef, cells=part, t=6)
 
-rf.pred <- PredictionList(models=rf.models, newdata=test.data)
-rf.perf <- PerformanceList(pred=rf.pred, measure="rch")
-
-## figure 5
-p <- plot(list(glm=glm.perf, rpart=rpart.perf, rf=rf.perf),
-          layout=c(3,1),
-          aspect="iso",
-          xlab=list(label="False Alarms/(False Alarms + Correct Rejections)", cex=0.6),
-          ylab=list(label="Hits/(Hits + Misses)", cex=0.6),
-          scales=list(x=list(tck=0.6), y=list(tck=0.6), cex=0.6),
-          key.args=list(cex=0.3, size=1.5),
-          par.strip.text=list(cex=0.6),
-          par.settings=list(strip.background=list(col="lightgrey")))
-p
-
-## trellis.device(device="pdf", width=4.72, height=3, file="f05_roc.pdf", family="Courier")
-## print(p)
-## dev.off()
-
-## test ability of models to predict location of urban gain 1985 -> 1991
-part <- rasterToPoints(obs[[1]], fun=function(x) x != 2, spatial=TRUE)
-test.data <- getPredictiveModelInputData(obs=obs, ef=ef, cells=part, t=6)
-
-glm.pred <- PredictionList(models=glm.models[[2]], newdata=test.data)
+glm.pred <- PredictionList(models=glm.mods[2], newdata=test.data)
 glm.perf <- PerformanceList(pred=glm.pred, measure="rch")
 
 ## figure 6
@@ -136,65 +147,70 @@ p
 ## dev.off()
 
 ## obtain demand scenario
-dmd <- approxExtrapDemand(obs=obs, tout=0:14)
+dmd <- approxExtrapDemand(lu=lu, tout=0:14)
 
 ## plot demand scenario (figure not shown in paper)
 matplot(dmd, type="l", ylab="Demand (no. of cells)", xlab="Time point", lty=1, col=c("Green","Red","Blue"))
-legend("topleft", legend=obs@labels, col=c("Green","Red","Blue"), lty=1)
+legend("topleft", legend=lu@labels, col=c("Green","Red","Blue"), lty=1)
 
 ## neighbourhood values
 w <- matrix(data=1, nrow=3, ncol=3)
-nb <- NeighbRasterStack(x=obs[[1]], weights=w, categories=c(1,2,3))
+nb <- NeighbRasterStack(x=lu[[1]], weights=w, categories=c(1,2,3))
 
 ## create CLUE-S model object
-clues.rules <- matrix(data=1, nrow=3, ncol=3, byrow=TRUE) 
-
-clues.parms <- list(jitter.f=0.0002,
-                    scale.f=0.000001,
-                    max.iter=1000,
-                    max.diff=50, 
-                    ave.diff=50) 
-
-clues.model <- CluesModel(obs=obs,
-                          ef=ef,
-                          models=glm.models,
+clues.model <- CluesModel(observed.lulc=lu, 
+                          explanatory.variables=ef,
+                          predictive.models=glm.mods,
                           time=0:14,
                           demand=dmd,
-                          elas=c(0.2,0.2,0.2),
-                          rules=clues.rules,
-                          params=clues.parms)
+                          history=NULL,
+                          mask=NULL,
+                          neighbourhood=NULL,
+                          transition.rules=matrix(data=1, nrow=3, ncol=3),
+                          neighbourhood.rules=NULL,
+                          elasticity=c(0.2,0.2,0.2),
+                          iteration.factor=0.00001,
+                          max.iteration=5000,
+                          max.difference=5,
+                          ave.difference=5)
+clues.result <- allocate(clues.model)
 
-## Create Ordered model (Fuchs et al)
-ordered.model <- OrderedModel(obs=obs,
-                              ef=ef,
-                              models=glm.models,
-                              time=0:14,
-                              demand=dmd,
+## create Ordered model (Fuchs et al)
+ordered.model <- OrderedModel(observed.lulc=lu, 
+                              explanatory.variables=ef, 
+                              predictive.models=glm.mods, 
+                              time=0:14, 
+                              demand=dmd,                              
+                              transition.rules=matrix(data=1, nrow=3, ncol=3),                              
                               order=c(2,1,3)) 
+ordered.result <- allocate(ordered.model, stochastic=TRUE)
 
-## perform allocation
-clues.model <- allocate(clues.model)
-ordered.model <- allocate(ordered.model, stochastic=TRUE)
+## evaluate model result
+## ordered
+ordered.tabs <- ThreeMapComparison(x=lu[[1]],
+                                   x1=lu[[3]],
+                                   y1=ordered.result[[15]],
+                                   factors=2^(1:8), 
+                                   categories=lu@categories,
+                                   labels=lu@labels) 
 
-## validate ordered model input
+ordered.agr  <- AgreementBudget(x=ordered.tabs) 
+ordered.fom  <- FigureOfMerit(x=ordered.tabs) 
+
+plot(ordered.agr, from=1, to=2) 
+plot(ordered.fom, from=1, to=2) 
 
 ## CLUE-S
-clues.tabs <- ThreeMapComparison(x=clues.model,
+clues.tabs <- ThreeMapComparison(x=lu[[1]],
+                                 x1=lu[[3]],
+                                 y1=clues.result[[15]],
                                  factors=2^(1:8),
-                                 timestep=14)
+                                 categories=lu@categories,
+                                 labels=lu@labels)
 
 ## plot three dimensional tables in different ways (figures not shown in paper)
 plot(clues.tabs)
 plot(clues.tabs, category=1, factors=2^(1:8)[c(1,3,5,7)])
-
-## Ordered
-ordered.tabs <- ThreeMapComparison(x=ordered.model,
-                                   factors=2^(1:8),
-                                   timestep=14)
-
-## as above for CLUE-S model
-plot(ordered.tabs)
-plot(ordered.tabs, category=1, factors=2^(1:9)[c(1,3,5,7)])
 
 ## calculate agreement budget and plot
 
@@ -210,10 +226,9 @@ p1 <- plot(clues.agr,
            ylim=c(0,0.08),
            scales=list(y=list(at=c(seq(from=0,to=0.08,by=0.02)), cex=0.6, tck=0.6), x=list(cex=0.6, tck=0.6)),
            key=list(cex=0.6))
-p1
+## p1
 
 ## Ordered
-ordered.agr <- AgreementBudget(x=ordered.tabs)
 p2 <- plot(ordered.agr,
            from=1,
            to=2,
@@ -224,17 +239,15 @@ p2 <- plot(ordered.agr,
            ylim=c(0,0.08),
            scales=list(y=list(at=c(seq(from=0,to=0.08,by=0.02)), cex=0.6, tck=0.6), x=list(cex=0.6, tck=0.6)),
            key=list(cex=0.6))
-p2
+## p2
 
 ## figure 7
 agr.p <- c("CLUE-S"=p1, Ordered=p2, layout=c(1,2))
 agr.p
 
-## trellis.device(device="pdf", width=4.72, height=5.72, file="/home/simon/projects/lulccR_paper/RevisedDraft/f07_agreement.pdf")
+## trellis.device(device="pdf", width=4.72, height=5.72, file="f07_agreement.pdf")
 ## print(agr.p)
 ## dev.off()
-
-## calculate Figure of Merit and plot
 
 ## CLUE-S
 clues.fom <- FigureOfMerit(x=clues.tabs)
@@ -251,7 +264,6 @@ p1 <- plot(clues.fom,
 p1
 
 ## Ordered
-ordered.fom <- FigureOfMerit(x=ordered.tabs)
 p2 <- plot(ordered.fom,
            from=1,
            to=2,
@@ -267,7 +279,6 @@ p2
 fom.p <- c("CLUE-S"=p1, Ordered=p2, layout=c(1,2))
 fom.p
 
-## trellis.device(device="pdf", width=4.72, height=4.72, file="/home/simon/projects/lulccR_paper/RevisedDraft/f08_figure_of_merit.pdf")
+## trellis.device(device="pdf", width=4.72, height=4.72, file="f08_figure_of_merit.pdf")
 ## print(fom.p)
 ## dev.off()
-
